834d9000a239a65eb059f7963e9db0ada5f99395
yuanzhoulvpi <yuanzhoulvpi@outlook.com>
2022-12-12 19:37:02 +0800

    init

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from transtext import *
 import pyperclip
 import time
 import pyautogui as pag
+from tqdm import tqdm
 
 
 def main():
@@ -12,6 +13,8 @@ def main():
     # 如果想要使用chatgpt
     # pt = ProcessTextChatGpt()
 
+    mulitg = CardLocation(config=CardConfig(template_file="images/image_06_groupat.png", bias_type=BiasType.NoBias))
+
     # 定位到被提到的位置
     ml = CardLocation(config=CardConfig(template_file="images/image_01_mention.png", bias_type=BiasType.NoBias))
     # 定位到被艾特的位置
@@ -19,14 +22,14 @@ def main():
     # 在被艾特的那个位置，右键，点击复制按钮
     cb = CardLocation(config=CardConfig(template_file="images/image_03_copy.png", bias_type=BiasType.NoBias))
     # 去消息回复框位置
-    it = CardLocation(config=CardConfig(template_file="images/image_04_input.png", bias_type=BiasType.HeightBias))
+    it = CardLocation(config=CardConfig(template_file="images/image_04_inputv2.png", bias_type=BiasType.HeightBias))
     # 点击发送按钮
     seb = CardLocation(config=CardConfig(template_file="images/image_05_send.png", bias_type=BiasType.NoBias))
 
-    limit_n = 3600  # 设置循环的上次次数
+    limit_n = 3600 * 8  # 设置循环的上次次数
     times = 0
     sec_value = 1  # 每隔1秒，就去扫描是否被提到，如果被提到就逐步进行下面步骤
-
+    pbar = tqdm(total=limit_n, desc="--->")
     while True:
         if times > limit_n:
             break
@@ -55,24 +58,46 @@ def main():
                 if cbv.status:
                     pag.moveTo(x=cbv.x, y=cbv.y)
                     pag.click()
-
-                    # 点击复制按钮
-                    # 获得复制的文本
-
+                    # 这里其实已经复制好了。
                     copy_text_value = pyperclip.paste()
+
+                    # 然后移动到头像中间，并且右键点击艾特
+                    back_x_value, back_y_value = 60, 30
+                    ## 这里是将鼠标移动到头像位置
+                    pag.moveTo(x=cbv.x - back_x_value, y=cbv.y - back_y_value)
+                    ## 在头像上，右键呼出艾特人选项
+                    pag.click(button='right')
+                    ## 呼出艾特人选项之后
+                    pag.moveTo(x=cbv.x - back_x_value + 2, y=cbv.y - back_y_value + 2)
+                    ## 点击艾特人的那个选项
+                    pag.click()
+                    # # pag.moveTo(x=cbv.x+200, y=cbv.y+200)
+
+                    # 这里是开始处理文本
                     clean_text_value = pt.trans(copy_text_value)
+                    # print(clean_text_value)
 
                     if clean_text_value.status:
+                        # 将处理好的文本，放到window系统的剪切板里面。
                         pyperclip.copy(clean_text_value.text)
+
+                        # 如果上方的pt.trans处理时间很短，就会遮盖住下面的模板，无法匹配内容（会导致找不到发消息的框），因此这里需要休息0.4秒
+                        time.sleep(0.4)
                         # 如何处理的文本没问题，那就继续
                         # 找到发送消息框
                         itv = it.get_location
+                        # print(itv.status)
                         if itv.status:
                             # 移动到发送框，点击
+                            at_other_x, at_other_y = 400, 0 # 这里位置上做了偏移
                             pag.moveTo(x=itv.x, y=itv.y)
+                            ## 因为被艾特之后，发送框里面会有被艾特的人的姓名，如果这个人的姓名太长的话，无法在文本输入框里面输入内容。
+                            pag.moveTo(x=itv.x + at_other_x, y=itv.y + at_other_y)
                             pag.click()  # 激活发送框
+                            # time.sleep(0.3)
+
                             pag.click(button='right')  # 呼出copy
-                            pag.moveTo(x=itv.x + 3, y=itv.y + 3)
+                            pag.moveTo(x=itv.x + at_other_x + 3, y=itv.y + at_other_y + 3)
                             pag.click()  # 点击copy 按钮
 
                             # 最后点击发送按钮
@@ -82,9 +107,16 @@ def main():
                                 pag.click()
                                 print("done ~")
 
+        else:
+            multigv = mulitg.get_location
+            if multigv.status:
+                pag.moveTo(x=multigv.x, y=multigv.y)
+                pag.click()
+
         time.sleep(sec_value)
-        times += sec_value
-        print(f"times: {times} / {limit_n}")
+        pbar.update(1)
+        # times += sec_value
+        # print(f"times: {times} / {limit_n}")
 
 
 if __name__ == '__main__':
--- transtext.py
+++ transtext.py
@@ -2,12 +2,24 @@ from datetime import datetime
 
 from dataclasses import dataclass
 from revChatGPT.revChatGPT import Chatbot
+import csv
+
+
+def append_question2file(question: str) -> None:
+    row = {'question': question,
+           'datetime': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+
+    with open(file="data/user_question.csv", mode='a', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=row.keys())
+        writer.writerow(row)
 
 
 @dataclass
 class ProcessTextOutput:
     status: bool = False
     text: str = None
+
+
 class ProcessText:
     """默认回复消息模板
     1. 基于此模板 可以检查整个流程是否跑通
@@ -22,11 +34,14 @@ class ProcessText:
 
     def trans(self, x: str) -> ProcessTextOutput:
         res = x.replace("@小埋 ", "")
+
+        append_question2file(question=res)
         if len(res) > 0:
 
             try:
+                notiy = "We're experiencing exceptionally high demand. Please hang tight as we work on scaling our systems."
 
-                res = f"[input]: {res}\n[datetime]: {datetime.now()}\n[result]: {res}"
+                res = f"\n[input]: {res}\n[time]: {datetime.now().strftime('%m-%d %H:%M:%S')}\n[question]：{res}\n[result]:{notiy}"
 
                 return ProcessTextOutput(
                     status=True,
@@ -34,9 +49,10 @@ class ProcessText:
                 )
             except Exception as e:
                 print("error 1")
+
                 return ProcessTextOutput(
                     status=True,
-                    text='error le'
+                    text='\nsorry 我出错了~'
                 )
         else:
             return ProcessTextOutput()
@@ -51,30 +67,43 @@ class ProcessTextChatGpt:
     1. 因为我的机器人叫小埋，用户在被艾特的时候，会包含你机器的名字，建议在处理文本的时候，抹掉机器人名称和@符号
 
     """
+
     def __init__(self) -> None:
         config = {
-            "session_token": "hello"
+            "session_token":"eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..TmvKhMBQM3GZCNR0.JA-B7qXeiAwA-gTWYr-SCpfIRQOqzEqYscYowKyizhj-7lEs0L5lur30k8l_JLgCqJgN8jggWh4mPZsT8Jj5o3P_rmYoY4CPts1bCgbZ3e0r0LaL1_IPqC4rjlZFvhz6K7ZiogqDz3OpdH5DrvH0n42x0wJxAbiRoCV-8xyeFqXZ1zyODn7JSZQc47ae_dzBx4wXIOEjJvgG3zG_U2cCCE6H-fI7NyGRA9IaRt-20iw1ve1QmVOWkgKPpz7NgdredDa0F0zlgO88wFZt-CzPZo0HmaczEGzZocwYmcdaEBET6maFTKwzsjBHkyHYz2suerpjOozAhcgPwlzO0P9Mt4KghfcBIhwyATIl8onpE7t_jGznKRWLrLyTPzJVXlKtjcv97-1guItdfxnGcDlGs7emsqofTzWPdZQTWmjvcjaWIFTpLLygPSY1qMXaMYBQlul9Cd0k9wrYfZ5ifUx3jHZfrwGToCy56AsnbdKT20ic4CO8NMQXL31-asBN_Y1EoTBZoz7511_sqfaEbjQNfmjZGRc21_F3BFas6w0MGghCajh_if5U3lderCb9ulRrFu9YHSCBcE2xMaDolY7nsQAdGQabOqBcQD0nhYMJw1RqS3YcNVCc79o5-CLRTLixNkJXGFHuWfVvdwH8u1EWdAmDnffiQMX0bP2j8JmXU2DtPzxxKzRjE1axGsRUHEVhiFi3PoO2PZjxFPg_RAZXvJ5E-pFMr1090Pjxe-br8_odI5lzUykqHcdwZub5sNpSoya1GkNhFVw67HQ4onRQoJ45Tup3gk80pWziSczmEY2HzeSxMY2LdjFzCglaUxcRFl4bPDmqms-2YTS5qaVGwEHlpXfwSQE0SCW6llfnYYlewkeG_83AspqwOokpUCqE2kN8EBVARlPEKChKTVA2YNr5Jq3VxVARlSB5BiYkquivMHy3shoDWeCNdKIytJM9nYrsGN3Zyamnr0FLKNrGLld9DzH4VMFOU-BdqK1nosyMVf6UpAglmwFoQgDMlTo_Fjsm_cOyhEMz27uRpd_tJ8AbjgnvwLkI6qN1iCYeqiJR0Fcy1df2BExxYpeSFICFNrKY7T5M_bXuejx1yGuTbwVZZOszWCqTTTAf6TcnIWs4NFiaRMESHwyF0aDtoGTDdxV7bGJ-GkjB2W-8HboA0VepC-6wuwoznet430_2doWVlrbHIoRYFFqpSL2L3T-rwatEASCzKN5jgxubEGayFmjYXa-8NI57-ddOeIYizrgxH0zkpp2AfxALYNTjvZJMowVhDaIBhT_-WlS53VUI2J25hoceUhtQZ_5BQycnsdpmx2LcTKNA1f0kB7I-uya_jfpyRe3jVaNX3txA8R0EaVVesvx3B2gM8jy_ph4yikk3X7it4eNUbozsQ4xNW3YtDBtGIS5R2aMXo0oQUh2sTNRBzYkEICoaWvUVCgDLjdsC6PkplRaT8DGXWsKyP6e_Moz0pKPwHCw0UKCqkQQ1X_cNntiDBRaP7_O13UkYD2RbrwrRCuYjeuf2dRYy4sAxKvv6SHdGojSO-q3sBWLLYGNoNuVTb9YgzZYZoxvokOsi1CGmsVkEc-cb-tjWf70_-ibkdbezhs6sRfrjM6bv23y-HaeEsmGt2rGzqR3xxDPFslQCRC97mw86VqcD3mx-1uutqAXvC-JYldXLP-4BCNVQB_7h9HsSeG_FUG4Mg3MD_JR6qUmSHP8rWbyph_33myqZtM27qfK8p35HBClvM89m9F5ydjG8pg2o-tIaQJFVQiBbne8BDMjgw-LohY7KBOazracwVxzDsICsFH0UHW6lZAFj1-bNBcWfCLxgZX7qNa2FTw6PeFUnw94S22yoPyXEVhUlF3rbaZv_NFw_KdC-_AceL10dZQud4QphQhsvlvtt_BdgamKncuKcgsIkfJdjsXILFxn732OdTnZpepvLiqvf58-PTdAOHu_JKqEh4VmcyO1sjS2uSbSzNLkVuwPya_MxbwLVhQWpbShxfEgTy4s6qyDV98Ev8TxTP1WHolhKu7BRXRiUBMZRjCIGKe0qG_zeuRMsX6s-ZNrzCwxe2ozKL0mYrVEso5BSfMpqf07H4-6EnGBFVmFHepvE1t67keYJIiWTip_V4rgs0V8R16a9mqMys95sWEyZmskZuOizqj4UXNw1g1XCtAP4AcXitLJkDb1Bnm4rsbp7Bl8SvO6PWJ5CVdr0mAdU5SbYzFukofE9XS9JZzFrdylvmOqrNtOTaCOMLTZ37o2qFu_NZNGDuNDWoGMdb1oSkegnotxiPtwfnfZX46pUH8DZzGGT6cUi6odwty8IK3NKxWgTj0TA0RLfEwVzwKnGcWB2lLQSZ28k6ZpL7fIqVDWA8WerbzvOJHz8ULbFjw.ETDCtnsgjMrYqNr9xv2kpw"
+            # "session_token":
+
         }
         self.chatbot = Chatbot(config, conversation_id=None)
 
     def trans(self, x: str) -> ProcessTextOutput:
         res = x.replace("@小埋 ", "")
+        append_question2file(question=res)
+
         if len(res) > 0:
 
             try:
                 response = self.chatbot.get_chat_response(res, output="text")
 
-                res = f"[input]: {res}\n[datetime]: {datetime.now()}\n[result]: {response.get('message', 'empty')}"
+                res = f"\n[input]: {res}\n[datetime]: {datetime.now()}\n[result]: {response.get('message', 'empty')}"
 
                 return ProcessTextOutput(
                     status=True,
                     text=res
                 )
             except Exception as e:
+                print(e)
+                res = f"\n[input]: {res}\n[datetime]: {datetime.now()}\n[result]: sorry~ 我出错了~"
+
                 print("error 1")
                 return ProcessTextOutput(
                     status=True,
-                    text='error le'
+                    text=res
                 )
         else:
             return ProcessTextOutput()
+
+
+if __name__ == '__main__':
+    pt = ProcessTextChatGpt()
+    pt.trans("nihao")
